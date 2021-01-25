5dae821aab0f33bcab860533fc023cd4781e6cdb
Beth Hollingsworth <travelerschecks@gmail.com>
2021-01-24 20:47:53 -0600

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -7,10 +7,10 @@ Local Settings for a rya_ebooks account.
 # Configuration for Twitter API
 ENABLE_TWITTER_SOURCES = True # Fetch twitter statuses as source
 ENABLE_TWITTER_POSTING = True # Tweet resulting status?
-MY_CONSUMER_KEY = 'lZ62tI9NIgY3rFjBGzYDzdrii' #Your Twitter API Consumer Key
-MY_CONSUMER_SECRET = '0UMQYAzjD8t56eXTRN6LMrMJxkfex1fKGI4gosBFtgnwQ9nKhN' #Your Consumer Secret Key
-MY_ACCESS_TOKEN_KEY = '4720135352-6TfOK5PvniFD2BitOc3CVyddaMJk7o2stgABqAC' #Your Twitter API Access Token Key
-MY_ACCESS_TOKEN_SECRET = 'uBxcP50aGdXBoD3zFvg2mzfbTh2Gpwe75EPhvyCgnRnob' #Your Access Token Secret
+MY_CONSUMER_KEY = 'xBzlEBTXdSGkvwq2YJMJAbEkk' #Your Twitter API Consumer Key
+MY_CONSUMER_SECRET = 'M64sJGmcTWjIrfTLYvF9fUi77KXnuZXcuOu8VMmse3olMIKUgh' #Your Consumer Secret Key
+MY_ACCESS_TOKEN_KEY = '1098655684151910405-LOYWFrmLQAHljrd9VjSpucYLpgvE59' #Your Twitter API Access Token Key
+MY_ACCESS_TOKEN_SECRET = 'HWZRb3LE9HUTyc10PWoeGu0UswT9d0TJ9bjerpD0wg514' #Your Access Token Secret
 
 # Configuration for Mastodon API
 ENABLE_MASTODON_SOURCES = False # Fetch mastodon statuses as a source?
@@ -20,7 +20,7 @@ CLIENT_CRED_FILENAME = '' # the MASTODON client secret file you created for this
 USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = ["@wheredthebotgo"] ["account", "names"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+TWITTER_SOURCE_ACCOUNTS = ["@hauntedlunchbox"] # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
@@ -34,7 +34,7 @@ ODDS = 0  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
 DEBUG = False  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = "account_without_@"  # The name of the account you're tweeting to.
+TWEET_ACCOUNT = "falloutebooks"  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
 TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
